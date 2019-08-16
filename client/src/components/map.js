a8c90dc99513fa394e7dba071a75b86c46558dd5
Ryan Creveling <ryancreveling@gmail.com>
2019-08-15 21:31:13 -0400

    initial commit for front end work

---
+++ client/src/components/map.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import GoogleMapReact from 'google-map-react';
+import { makeStyles } from '@material-ui/core/styles';
+
+const text = "Wow wow wow"
+const MarkerComponent = ({ text }) => <div>{text}</div>;
+
+class Map extends Component {
+    static defaultProps = {
+        center: {
+            lat: 59.95,
+            lng: 30.33
+        },
+        zoom: 9
+    };
+
+    render() {
+        return (
+            // Important! Always set the container height explicitly
+            <div style={{ height: '100vh', width: '100%' }}>
+                <GoogleMapReact
+                    bootstrapURLKeys={{ key: 'AIzaSyCiZ-jsILS_LD8OOFCvlybQvnvyjb1jtaQ' }}
+                    defaultCenter={this.props.center}
+                    defaultZoom={this.props.zoom}
+                >
+                    <MarkerComponent
+                        lat={59.955413}
+                        lng={30.337844}
+                        text="My Marker"
+                    />
+                </GoogleMapReact>
+            </div>
+        );
+    }
+}
+
+export default Map;

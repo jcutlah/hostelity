f8a446b7d3fa71df3dc893899a0280f3582f8f27
James Cutler <jcutler@hubspot.com>
2019-08-17 23:29:41 -0400

    added and configured login route for adding new user

--- routes/api/login.js
+++ routes/api/login.js
@@ -2,13 +2,24 @@ const router = require("express").Router();
 const userController = require("../../controllers/userController");
 
 // Matches with "/api/hostels"
-router.route("/api/users")
+router.route("/users")
   .get(function(req, res){
       console.log(req);
       console.log(res);
   })
-  .post(userController.addUser);
+//   .post(userController.addUser);
 
+router.route('/signup')
+  .get(function(req, res){
+      console.log(req.data);
+    })
+  .post(function(req, res){
+      console.log(`request received`);
+      console.log(req.body);
+      userController.addUser(req.body,function(user){
+          res.json(user);
+      })
+  })
 
 
 module.exports = router;

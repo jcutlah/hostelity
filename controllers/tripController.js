e6598eeb8784dfb76204c6c560323136b410820e
James Cutler <jcutler@hubspot.com>
2019-08-17 22:39:21 -0400

    code cleanup

--- controllers/tripController.js
+++ controllers/tripController.js
@@ -22,15 +22,3 @@ const orm = {
 };
 
 module.exports = orm;
-
-// addNote: function(message,callback){
-//     console.log('running addNote()');
-//     db.Note.create({comment:message.message.message}).then(function(result){
-//         return db.Article.findOneAndUpdate({ _id: message.id }, {$push:{ notes: result._id }});
-
-//     }).then(function(result){
-//         callback(result);
-//     }).catch(err => {
-//         callback(err);
-//     });
-// },

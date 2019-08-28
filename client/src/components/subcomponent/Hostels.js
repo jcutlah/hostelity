27fce917eb63f916079603bfea32c947c59db420
James Cutler <jcutler@hubspot.com>
2019-08-28 04:47:54 -0400

    Added component for Waypoints and restructured Trip/Hostel components to account for data structure changes

--- client/src/components/subcomponent/Hostels.js
+++ client/src/components/subcomponent/Hostels.js
@@ -16,19 +16,7 @@ const Hostels = (props) => {
                         <Typography align="center" component="p">
                             {hostel.title}
                         </Typography>
-                       
-
-                        <Typography variant="h6" gutterBottom>
-                            Location:
-                        </Typography>
-
-                        <Typography component="p">
-                            {hostel.location[0]}
-
-                            {hostel.location[1]}
-                        </Typography>
-                       
-
+                        <hr></hr>
                     </div>
                 )
             })}
--- client/src/components/subcomponent/Trips.js
+++ client/src/components/subcomponent/Trips.js
@@ -1,8 +1,10 @@
 import React from 'react';
-import Hostels from './Hostels';
 import { makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
 import Typography from '@material-ui/core/Typography';
+import Waypoints from './Waypoints';
+import Axios from 'axios';
+
 
 
 const useStyles = makeStyles(theme => ({
@@ -11,41 +13,43 @@ const useStyles = makeStyles(theme => ({
     },
 }));
 
+
 const Trips = (props) => {
-    console.log(props);
+    console.log(typeof props.trips);
+    props.trips.length ? 
+        console.log(typeof props.trips) :
+        console.log('meep meep');
+    
     const classes = useStyles();
+    
+
     return (
         <Paper className={classes.root}>
+            {console.log(typeof props.trips)}
             {props.trips.map(trip => {
-                console.log(trip);
-                console.log(trip.hostels);
+
                 return (
                     <div key={trip._id} className="tripSummary">
                         <Typography variant="h5" component="h3" align="center">
                             {trip.name}
                         </Typography>
                         <hr></hr>
-                        <Hostels
-                            hostels={trip.hostels}
-                        />
 
-                        <Typography variant="h6" gutterBottom>
+                        <Typography className="stat" variant="h6" gutterBottom>
                             Start:
                          </Typography>
-                       
-                        <Typography component="p">
-                            {trip.startDest.name}
+                        <Typography className="stat" component="p">
+                            {trip.start}
                         </Typography>
-                      
-
-                        <Typography variant="h6" gutterBottom>
+                        <Typography className="stat" variant="h6" gutterBottom>
                             End:
                         </Typography>
-                        
-
-                        <Typography component="p">
-                            {trip.endDest.name}
+                        <Typography className="stat" component="p">
+                            {trip.end}
                         </Typography>
+                        <Waypoints
+                            waypoints={trip.waypoints}
+                        />
                     </div>
 
                 )

5d91a831a7ecd882ae1d4f091a99b74e431e12ec
James Cutler <jcutler@hubspot.com>
2019-08-28 04:54:28 -0400

    new route for waypoints

--- routes/api/index.js
+++ routes/api/index.js
@@ -2,9 +2,11 @@ const router = require("express").Router();
 const loginRoutes = require('./login');
 const tripRoutes = require('./trips');
 const mapRoutes = require('./maps');
+const waypointRoutes = require('./waypoints');
 
 router.use('/users', loginRoutes);
 router.use('/trips', tripRoutes);
 router.use('/maps', mapRoutes);
+router.use('/waypoints', waypointRoutes);
 
 module.exports = router;

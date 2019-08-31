0fbd3a41b69cabbe051c9f8e0054163061040027
James Cutler <jcutler@hubspot.com>
2019-08-31 15:45:57 -0400

    added response to trip post route

--- routes/api/trips.js
+++ routes/api/trips.js
@@ -12,13 +12,14 @@ router.route("/:id")
         });
     })
     .post(function(req, res){
-        console.log("post request made to /api/trips/:id");
-        tripController.addTrip(req.body, function(trip){
-            tripController.associateTripToUser(req.params.id, trip._id, function(user){
-                res.json(trip);
-                });
-            });
-        })
+        console.log("post request made to /api/trips");
+        res.json({message: "success, boyyyeeeee!!!"});
+        // tripController.addTrip(req.body, function(trip){
+        //     tripController.associateTripToUser(req.params.id, trip._id, function(user){
+        //         res.json(trip);
+        //         });
+        //     });
+        // })
     .put(function(req, res){
         console.log("put request made to /api/trips/:id");
         tripController.updateTripWithWaypoint(req.params.id, req.body.wayPointId, function(trip){

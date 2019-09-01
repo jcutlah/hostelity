61c3519c43a51398e60aabec3a123ed1c961e82f
James Cutler <jcutler@hubspot.com>
2019-09-01 07:51:15 -0400

    successfully taking trip data, with waypoints, from the map, and adding to database

--- routes/api/tripsAPI.js
+++ routes/api/tripsAPI.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const tripController = require("../../controllers/tripController");
-
+const waypointController = require("../../controllers/waypointController");
 
 // Matches with "/api/trips/:id"
 router.route("/:id")
@@ -22,7 +22,43 @@ router.route("/:id")
         .post(function(req, res){
             console.log("post request made to /api/trips");
             console.log(req.body);
-            console.log(req.body.waypoints[0].location.lat());
+            let totalDistance = 0;
+            const waypoints = req.body.waypoints;
+            waypoints.forEach(point => {
+                totalDistance += parseInt(point.distance);
+            })
+            console.log(totalDistance);
+            const tripObject = {
+                start: req.body.start.name,
+                end: req.body.end.name,
+                name: "My Journey",
+                totalMileage: totalDistance
+            }
+            tripController.addTrip(tripObject, function(trip){
+                const tripId = trip._id;
+                console.log(tripId);
+                waypoints.forEach((point, i) => {
+                    let waypoint = {
+                        name: point.name,
+                        trip: tripId,
+                        tripIndex: i,
+                        distanceToWaypoint: parseInt(point.distance),
+                        location: {
+                            type: "Point",
+                            coordinates: [point.location[1],point.location[0]]
+                        }
+                    }
+                    waypointController.addWaypoint(waypoint, function(wp){
+                        console.log(wp);
+                        tripController.updateTripWithWaypoint(tripId, wp._id, function(trip){
+                            console.log(trip);
+                        })
+                    })
+                })
+                tripController.associateTripToUser(req.user, tripId, function(user){
+                    console.log(user);
+                })
+            })
             res.json({message: "success, boyyyeeeee!!!"});
             // tripController.addTrip(req.body, function(trip){
             //     tripController.associateTripToUser(req.params.id, trip._id, function(user){

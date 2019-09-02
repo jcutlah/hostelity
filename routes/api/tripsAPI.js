fc6c3fea714b7d10ec4fbc8612168098cd791859
James Cutler <jcutler@hubspot.com>
2019-09-02 00:33:05 -0400

    hostels being associated to waypoints based on distance.

--- routes/api/tripsAPI.js
+++ routes/api/tripsAPI.js
@@ -1,6 +1,7 @@
 const router = require("express").Router();
 const tripController = require("../../controllers/tripController");
 const waypointController = require("../../controllers/waypointController");
+const hostelController = require("../../controllers/hostelController");
 
 // Matches with "/api/trips/:id"
 router.route("/:id")
@@ -27,14 +28,15 @@ router.route("/:id")
         })
         .post(function(req, res){
             console.log("post request made to /api/trips");
-            console.log(req.body);
+            // console.log(req.body);
             let totalDistance = 0;
             const waypoints = req.body.trip.waypoints;
             const trip = req.body.trip;
+            const hostels = req.body.hostels;
             waypoints.forEach(point => {
                 totalDistance += parseInt(point.distance);
             })
-            console.log(totalDistance);
+            // console.log(totalDistance);
             const tripObject = {
                 start: trip.start.name,
                 end: trip.end.name,
@@ -56,14 +58,38 @@ router.route("/:id")
                         }
                     }
                     waypointController.addWaypoint(waypoint, function(wp){
-                        console.log(wp);
+                        // console.log(wp);
                         tripController.updateTripWithWaypoint(tripId, wp._id, function(trip){
-                            console.log(trip);
+                            // console.log(trip);
                         })
                     })
+                    if (i === waypoints.length -1){
+                        console.log("done adding waypoints. adding hostels...");
+                        hostels.forEach(hostel => {
+                            console.log(hostel);
+                            hostelController.addHostel(hostel, function(result){
+                                console.log('newly added hostel shiz:');
+                                console.log(result);
+                                waypointController.findClosestWaypointToHostel(tripId, result.location.coordinates, 10000, function(closestWaypoint){
+                                    console.log('closest waypoint found?');
+                                    console.log(closestWaypoint);
+                                    hostelController.associateHostelToWaypoint(closestWaypoint._id, result._id, function(response){
+                                        console.log('updated waypoint with Hostel: ');
+                                        console.log(response);
+                                    })
+                                })
+                            })
+                        })
+                    }
                 })
                 tripController.associateTripToUser(req.user, tripId, function(user){
                     console.log(user);
+                })
+                // loop through hostels, find the closest waypoint, add and associate hostel to that waypoint.
+                hostels.forEach(hostel => {
+                    console.log('newly added hostel shiz');
+                    console.log(hostel);
+
                 })
             })
             res.json({message: "success, boyyyeeeee!!!"});

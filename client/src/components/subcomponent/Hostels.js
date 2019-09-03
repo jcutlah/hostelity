9522fab137e240d279c5c9e0966240f23d6ecdd8
Ryan Creveling <ryancreveling@gmail.com>
2019-09-02 22:03:14 -0400

    end of night

--- client/src/components/subcomponent/Hostels.js
+++ client/src/components/subcomponent/Hostels.js
@@ -3,6 +3,7 @@ import Typography from '@material-ui/core/Typography';
 import Card from '@material-ui/core/Card'
 import CardMedia from '@material-ui/core/CardMedia'
 import { makeStyles } from '@material-ui/core/styles';
+import Button from "@material-ui/core/Button";
 
 var useStyles = makeStyles(theme => ({
     media: {
@@ -16,6 +17,7 @@ const Hostels = (props) => {
     return (
         <div className="tripList section">
             {props.hostels.map(hostel => {
+                var thisAddressSearch = 'http://www.google.com/maps?q=' + hostel.address
                 console.log(hostel)
                 return (
                     <div>
@@ -33,7 +35,8 @@ const Hostels = (props) => {
                                     <Typography align="center" component="p">
                                         {hostel.title}
                                         <br />
-                                        <span align="right">{hostel.address}</span>
+                                        <Button><a target='_blank' href={thisAddressSearch} className="hostelLink" align="right">{hostel.address}</a></Button>
+
                                     </Typography>
                                 </Typography>
 

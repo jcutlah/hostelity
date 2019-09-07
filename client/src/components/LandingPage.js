22bed2829a367ec0f63dc25f671c5e060b4e2208
jimbomoso <jimbomoso@gmail.com>
2019-09-07 12:48:51 -0400

    added text and screenshots to landingpage

--- client/src/components/LandingPage.js
+++ client/src/components/LandingPage.js
@@ -24,19 +24,22 @@ const LandingPage = (props) => {
     return (
         <Grid container direction="column" justify="center" alignItems="center" spacing={5} className={classes.container}>
 
-            <Grid item xs={12} align='left'>
-                <img src='/assets/images/switchBakLogo.png'></img>
-            </Grid>
             <Grid item xs={12} align="right">
                 <div className="landingText">
-                    I once knew a man from a town they called Began, And He tried to throw a fit
-                    he did it, he did, but lost the liquid - that runs through his veins and flows red
+                    switchBak is THE app for everyone who dreams about traveling the world, but isn’t sure where to start or how to afford it.  switchBak takes the guesswork out of economical travel planning by providing an easy way to find the world’s best hostels and to keep track of trip itineraries.
                 </div>
+            </Grid>
+            <Grid item xs={12} align='left'>
+                <h2> Simply enter where you want to go</h2>
+                <img src='/assets/images/tripInput.png' width='500px'></img>
+                <h2> then click begin to get started on your next adventure</h2>
+                <img src='/assets/images/tripInputMap.png' width='500px'></img>
             </Grid>
             <br />
 
             <Grid item align='right'>
-                <img src='/assets/images/switchBakLogo.png'></img>
+                <h2> Click on a waypoint see the best hostels in that area </h2>
+                <img src='/assets/images/tripWaypointZoomed.png' width='500px'></img>
             </Grid>
             <Grid item align="left">
                 <div className="landingText">
